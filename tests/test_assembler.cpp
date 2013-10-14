2325790ae86635e38b7495dc8e70923112e66dd8
Dana Christen <dana.christen@epfl.ch>
2013-10-14 12:27:55 -0700

    Asembler test update

--- tests/test_assembler.cpp
+++ tests/test_assembler.cpp
@@ -23,8 +23,10 @@
 
 #include "p1d_assembler.hpp"
 #include <cassert>
+#include <iostream>
 
 using namespace poisson1d;
+using namespace std;
 
 int main(int argc, char* argv[])
 {
@@ -32,33 +34,59 @@ int main(int argc, char* argv[])
     Real a(-1.0);
     Real b(1.0);
     UInt n(100);
-    std::string rhs_func("-x*x+1");
+    string rhs_func("-x*x+1");
 
     DistributedAssembler assembler(a, b, n, rhs_func);
 
-    assembler.assemble_rhs();
-    assembler.assemble_matrix();
+    Real* rhs_ptr = new Real[n];
+    Real* matrix_ptr = new Real[3*n];
 
-    const Real* rhs_ptr = assembler.get_rhs_ptr();
-    const Real* matrix_ptr = assembler.get_matrix_ptr();
+    assembler.assemble_rhs(rhs_ptr);
+    assembler.assemble_matrix(matrix_ptr);
 
     Real dx = (b-a)/n;
 
-    for (std::size_t i(0); i < n; ++i)
+    cout << "Testing rhs..." << endl;
+    for (size_t i(0); i < n; ++i)
     {
         Real expected_value = -(a+i*dx)*(a+i*dx)+1;
+        cout << " rhs_ptr[" << i << "] = " << rhs_ptr[i] << " expected " << expected_value << endl;
         assert(rhs_ptr[i] == expected_value);
     }
 
-    for (std::size_t i(0); i < 3*n; i+=3)
+    cout << "Testing matrix..." << endl;
+
+    Real expected_coef1 = -1/dx;
+    Real expected_coef2 = 2/dx;
+
+    assert(matrix_ptr[0] == expected_coef1);
+    cout << " matrix[0,0] = " << matrix_ptr[0] << " expected " << expected_coef1 << endl;
+
+    assert(matrix_ptr[1] == expected_coef2);
+    cout << " matrix[0,1] = " << matrix_ptr[1] << " expected " << expected_coef2 << endl;
+
+    for (size_t i(2); i < 3*n-2; i+=3)
     {
-        Real expected_coef1 = -1/dx;
-        Real expected_coef2 = 2/dx;
 
         assert(matrix_ptr[i] == expected_coef1);
+        cout << " matrix[" << i << "," << i-1 << "] = " << matrix_ptr[i] << " expected " << expected_coef1 << endl;
+
         assert(matrix_ptr[i+1] == expected_coef2);
+        cout << " matrix[" << i << "," << i << "] = " << matrix_ptr[i+1] << " expected " << expected_coef2 << endl;
+
         assert(matrix_ptr[i+2] == expected_coef1);
+        cout << " matrix[" << i << "," << i+1 << "] = " << matrix_ptr[i+2] << " expected " << expected_coef1 << endl;
+
     }
+
+    assert(matrix_ptr[3*n-2] == expected_coef1);
+    cout << " matrix[" << 3*n-1 << "," << 3*n-2 << "] = " << matrix_ptr[3*n-2] << " expected " << expected_coef1 << endl;
+
+    assert(matrix_ptr[3*n-1] == expected_coef2);
+    cout << " matrix[" << 3*n-1 << "," << 3*n-1 << "] = " << matrix_ptr[3*n-1] << " expected " << expected_coef2 << endl;
+
+    delete[] rhs_ptr;
+    delete[] matrix_ptr;
 
     return 0;
 }
